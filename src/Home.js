1926a19567c6e7f2616dc92e1ab580f46ab5de8d
ᗩTᗩᗰYᖇᗩT <komputeratamyrat@gmail.com>
2023-06-25 13:34:05 +0500

    1.0.2

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('hello, user')
+    }
+    const handleClickAgain = (name) => {
+        console.log('hello ' + name)
+    }
+
     return (
         <div className="home">
             <h1>Home page</h1>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => handleClickAgain('Atamyrat')}>Click me again</button>
         </div>
     );
 }
