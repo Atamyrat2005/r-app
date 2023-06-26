2122731dd6570381873ec79ad3b0668bf7bef1ed
ᗩTᗩᗰYᖇᗩT <komputeratamyrat@gmail.com>
2023-06-26 11:57:28 +0500

    1.0.4

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,19 @@
 import { useState } from "react";
 
 const Home = () => {
-    // let name = 'Atamyrat';
-    const [name, setName] = useState('Atamyrat');
-    const [age, setAge] = useState(25);
-
-    const handleClick = () => {
-        setName('Shukurov');
-        setAge(30);
-    }
-
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'lorem ipsum...', author: 'Meret', id: 1},
+        {title: 'Welcome', body: 'lorem ipsum...', author: 'Maksat', id: 2},
+        {title: 'Dev tools', body: 'lorem ipsum...', author: 'Myrat', id: 3},
+    ]);
     return (
         <div className="home">
-            <h1>Home page</h1>
-            <p>{name} is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
