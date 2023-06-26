4d1d4556540a9764077016d9cd64f6566e72324f
ᗩTᗩᗰYᖇᗩT <komputeratamyrat@gmail.com>
2023-06-26 17:13:16 +0500

    1.0.5

---
+++ src/BlogList.js
@@ -0,0 +1,28 @@
+// second method
+
+const BlogList = ({blogs, title}) => {
+
+    // second method end
+
+    // first method start
+
+// const BlogList = (props) => {
+    // const blogs = props.blogs;
+    // const title = props.title;
+
+    // first method end
+
+    return (
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by: {blog.author}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+ 
+export default BlogList;
