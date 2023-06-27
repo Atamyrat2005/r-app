6140263d1feab6c8c80e2a48b3917ec912af591c
ᗩTᗩᗰYᖇᗩT <komputeratamyrat@gmail.com>
2023-06-27 22:27:18 +0500

    1.0.8

--- src/Home.js
+++ src/Home.js
@@ -4,21 +4,31 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
+            if(!res.ok) {
+                throw Error('Could not fetch the data')
+            }
             return res.json();
         })
         .then(data => {
             setBlogs(data);
             setIsPending(false);
-        });
+            setError(null);
+        })
+        .catch(err => {
+            setIsPending(false);
+            setError(err.message);
+        })
     }, []);
 
 
     return (
         <div className="home">
+            { error && <div className="center">{ error }</div>}
             {isPending && <div className="center">Loading...</div>}
             {blogs  && <BlogList blogs={blogs} title="All Blogs!"></BlogList>}
         </div>
